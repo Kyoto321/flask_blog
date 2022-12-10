a1dea963344de7e485b8cad7f747ee1fbe665e29
Kyoto321 <bankoleayodele147@gmail.com>
2022-12-10 06:37:23 +0100

    Full flask website
    routes and templates

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,14 +1,40 @@
 # Initialize application and bring together different components
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_blog.config import Config
 
 
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '49cb7e2bfba717abc959512a2a0e74086f13d028'
-# config a database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # create a db instance
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_category = 'info'
 
-from flask_blog import routes
+mail = Mail()
+
+
+def create_app(config_classs=Config):
+	app = Flask(__name__)
+	app.config.from_object(Config)
+
+	db.init_app(app)
+	bcrypt.init_app(app)
+	login_manager.init_app(app)
+	mail.init_app(app)
+
+
+	from flask_blog.users.routes import users
+	from flask_blog.posts.routes import posts
+	from flask_blog.main.routes import main
+	from flask_blog.errors.handlers import errors
+	app.register_blueprint(users)
+	app.register_blueprint(posts)
+	app.register_blueprint(main)
+	app.register_blueprint(errors)
+
+	return app
